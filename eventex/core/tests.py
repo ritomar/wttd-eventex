5d8416fd41b4418a841a62e0d937d83e0d07d97c
ritomar <ritomar@hotmail.com>
2016-09-07 22:37:37 -0300

    Adiciona teste na home

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+""" HomeTest é um cenário de teste pois herda de TestCase """
+
+class HomeTest(TestCase):
+    def setUp(self):
+        # response é um atributo da classe HomeTest
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """ GET '/ must return status code 200 """
+        # self.client faz requisições HTTP diretas para o django
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """ Must use 'index.html' """
+        self.assertTemplateUsed(self.response, 'index.html')
